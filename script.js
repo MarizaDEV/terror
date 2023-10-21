1e781ce7dbecdbf5d204a9258d08ddf5045b6a1d
MarizaDEV <contatomarizalima@gmail.com>
2023-10-21 15:32:48 -0300

    alterações no menu e nova função slideshow

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "imagens/menu.svg";
+        document.querySelector('.icon').src = "imagens/menu.png";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "imagens/close.svg";
+        document.querySelector('.icon').src = "imagens/close.png";
     }
 }
